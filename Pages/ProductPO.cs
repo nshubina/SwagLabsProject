9c90ffc0f436265ad7d958905064575a5db798ad
Natalia Shubina <nshubinaqa@gmail.com>
2022-10-19 12:04:39 +0300

    Change return text in getTitle() and getItemName() methods

---
+++ Pages/ProductPO.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using SwagLabsProject.Initials;
+using SwagLabsProject.Login;
+using SwagLabsProject.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwagLabsProject.Pages
+{
+    public class ProductPO : PageBase
+    {
+        protected IWebDriver Driver;
+
+        private By _productTitle = By.CssSelector(".header_secondary_container .title");
+        private By _itemName = By.Id("item_4_title_link");
+
+        public ProductPO(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public string getTitle()
+        {
+            return GetText(_productTitle);
+        }
+
+        public string getItemName()
+        {
+            return GetText(_itemName);
+        }
+/*
+        public ProductDetailPO selectProduct()
+        {
+            Click(_itemName);
+            Driver.FindElement(product)
+            return new ProductDetailPO(Driver);
+        }
+   */
+    }
+}
