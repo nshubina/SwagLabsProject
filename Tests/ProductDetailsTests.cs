ec5e89cd7c49cc5e368e975623adf113cf572116
Natalia Shubina <nshubinaqa@gmail.com>
2022-10-21 14:54:09 +0300

    Add new test

---
+++ Tests/ProductDetailsTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using SwagLabsProject.Login;
+
+namespace SwagLabsProject.Tests
+{
+    public class ProductDetailsTests : BasePO
+    {
+        [Test]
+        public void When_ClickAddToCartButtonOnProductDetailsPage_ShouldItemAddToCart()
+        {
+            var loginPage = new LoginPO(Driver);
+            var productPage = loginPage.LoginWithCreds("standard_user", "secret_sauce");
+
+            var productDetailsPage = productPage.ClickOnProduct();
+            productDetailsPage.AddToCart();
+
+            int cartIndex = productDetailsPage.CardIndexChanged();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(productPage.GetRemoveButtonText(), "REMOVE");
+                Assert.Greater(cartIndex, 0);
+            });
+        }
+
+    }
+}
