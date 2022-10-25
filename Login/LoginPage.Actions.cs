8be6a1a95b12877f52f5c2c8524531e5a04ce71e
Natalia Shubina <nshubinaqa@gmail.com>
2022-10-25 17:36:42 +0300

    Implemented Page Factory for the LoginPage Divided the Login page into partial classes: Assert, Elements and Actions Add LogTest for testing Login Page Factory

---
+++ Login/LoginPage.Actions.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using SwagLabsProject.Pages;
+
+namespace SwagLabsProject.Login
+{
+    public partial class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void SetUsername(string uName)
+        {
+
+            _fldUsername.Click();
+            _fldUsername.Clear();
+            _fldUsername.SendKeys(uName); ;
+        }
+
+        public void SetPassword(string password)
+        {
+            _fldPassword.Click();
+            _fldPassword.Clear();
+            _fldPassword.SendKeys(password);
+        }
+
+        public ProductPO LoginWithCreds(string uName, string password)
+        {
+            SetUsername(uName);
+            SetPassword(password);
+            _btnLogin.Click();
+            return new ProductPO(driver);
+        }
+
+        public string GetErrorMessage()
+        {
+            return _errorLogin.Text;
+        }
+    }
+}
---
+++ Login/LoginPage.Elements.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SwagLabsProject.Login
+{
+    public partial class LoginPage
+    {
+        [FindsBy(How = How.Id, Using = "user-name")]
+        private IWebElement _fldUsername;
+
+        [FindsBy(How = How.Id, Using = "password")]
+        private IWebElement _fldPassword;
+
+        [FindsBy(How = How.Id, Using = "login-button")]
+        private IWebElement _btnLogin;
+
+        [FindsBy(How = How.XPath, Using = "//form/div[3]/h3")]
+        private IWebElement _errorLogin;
+    }
+}
