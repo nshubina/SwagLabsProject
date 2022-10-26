68d7dbbf96a593132da16564beeebd288d9e0392
Natalia Shubina <nshubinaqa@gmail.com>
2022-10-26 18:06:34 +0300

    Add methods to ProductPage

--- Pages PO/ProductDetails/ProductDetailsPage.Elements.cs
+++ Pages PO/ProductDetails/ProductDetailsPage.Elements.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
 
 namespace SwagLabsProject.Pages_PO.ProductDetails
 {
-    internal class ProductDetailsPage
+    public partial class ProductDetailsPage
     {
+        /* [FindsBy(How.CssSelector, Using = ".header_secondary_container .title")]
+         private IWebDriver _titleCategory;*/
+
+        [FindsBy(How = How.Id, Using = "item_0_title_link")]
+        private IWebElement _productName;
+
+        [FindsBy(How = How.Id, Using = "add-to-cart-sauce-labs-bike-light")]
+        private IWebElement _btnAddToCart;
+
+        [FindsBy(How = How.Id, Using = "remove-sauce-labs-bike-light")]
+        private IWebElement _btnRemoveProduct;
+
+        [FindsBy(How = How.Id, Using = "shopping_cart_link")]
+        private IWebElement _btnCartIconBtn;
     }
 }
