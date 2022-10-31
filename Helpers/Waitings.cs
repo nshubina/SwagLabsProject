8c9b345c802005bda6cb2083da24f9ff6a02f542
Natalia Shubina <nshubinaqa@gmail.com>
2022-10-31 08:22:20 +0200

    Create helpers folder Create Waitings.cs Create WaitUntil method.

---
+++ Helpers/Waitings.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+
+namespace SwagLabsProject.Helpers
+{
+    public class Waitings
+    {
+        public static void WaitUntil(IWebDriver driver, IWebElement element, int second)
+        {
+            var waiter = new WebDriverWait(driver, TimeSpan.FromSeconds(second));
+            try
+            {
+                waiter.Until(d =>
+                {
+                    if (element != null && element.Displayed)
+                    {
+                        return element;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                });
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
