aced48ad540e9c988e046387b2717ced1aebb707
Natalia Shubina <nshubinaqa@gmail.com>
2022-11-03 18:44:37 +0200

    Add new folder and checkout class Initiate elements use page factory add assertion and update locators

---
+++ PagesPO/Checkout/CheckoutPage.Actions.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SwagLabsProject.PagesPO.Checkout
+{
+    public partial class CheckoutPage
+    {
+        private IWebDriver _driver;
+
+        public CheckoutPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+    }
+}
---
+++ PagesPO/Checkout/CheckoutPage.Assertions.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace SwagLabsProject.PagesPO.Checkout
+{
+    public partial class CheckoutPage
+    {
+        public CheckoutPage AssertUserIsNavigatedToCheckoutPage()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(_headerYourInfo.Text, "CHECKOUT: YOUR INFORMATION");
+                Assert.That(_inputFirstName.Displayed);
+            });
+            return this;
+        }
+    }
+}
