ddc033bec1327e6ac711b74ade0cb504fadfd2c4
Natalia Shubina <nshubinaqa@gmail.com>
2022-11-03 12:44:38 +0200

    Add multiply assert (AssertAddedProductIsEqualToProductInTheCart)

--- PagesPO/Cart/CartPage.Assertions.cs
+++ PagesPO/Cart/CartPage.Assertions.cs
@@ -1,6 +1,17 @@
-﻿namespace SwagLabsProject.PagesPO.Cart
+﻿using NUnit.Framework;
+
+namespace SwagLabsProject.PagesPO.Cart
 {
     public partial class CartPage
     {
+        public CartPage AssertAddedProductIsEqualToProductInTheCart(string addedProductName)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(_headerTitle.Text, "YOUR CART");
+                Assert.AreEqual(addedProductName, _addedNameProduct.Text);
+            });
+            return this;
+        }
     }
 }
