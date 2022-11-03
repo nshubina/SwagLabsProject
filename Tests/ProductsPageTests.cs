4187f61cc5f5846db8afb7dbcd6e3417d64907f3
Natalia Shubina <nshubinaqa@gmail.com>
2022-11-03 10:53:28 +0200

    Refactoring Use Fluent pattern Add Descriptions

--- Tests/ProductsPageTests.cs
+++ Tests/ProductsPageTests.cs
@@ -3,9 +3,11 @@ using SwagLabsProject.PagesPO.Login;
 
 namespace SwagLabsProject.Tests
 {
+    [TestFixture]
     public class ProductsPageTests : BasePO
     {
         [Test]
+        [Description("User is navigated to the Product Details page, when click on product name")]
         public void WhenClickProductNameExpectProductDetails()
         {
             var loginPage = new LoginPage(Driver);
@@ -14,43 +16,46 @@ namespace SwagLabsProject.Tests
             var productName = productPage.GetProductName();
 
             var productDetailPage = productPage.ClickOnProduct();
-            var detailProductName = productDetailPage.GetDetailProductName();
+            var openedProductName = productDetailPage.GetDetailProductName();
 
-            productPage.ElementsAreAqualAssert(productName, detailProductName);
+            productPage.ElementsAreAqualAssert(productName, openedProductName);
         }
 
         [Test]
+        [Description("Product adds to cart after click on the 'Add to cart' btn")]
         public void WhenClickAddToCartBtnOnProductExpectProductAddsToCard()
         {
             var loginPage = new LoginPage(Driver);
-            var productPage = loginPage.LoginWithCreds("standard_user", "secret_sauce");
+            var productPage = loginPage.LoginWithCreds("standard_user", "secret_sauce")
+                .AddProductToCard();
 
-            productPage.AddProductToCard();
             productPage.ElementsAreAqualAssert(productPage.GetRemoveButtonText(), "REMOVE");
         }
 
         [Test]
+        [Description("The added product is removed from the cart after clicking the 'remove from cart' button")]
         public void WhenClickRemoveButtonOnProductExpectProductIsRemovedFromCart()
         {
             var loginPage = new LoginPage(Driver);
-            var productPage = loginPage.LoginWithCreds("standard_user", "secret_sauce");
-
-            productPage.AddProductToCard();
-            productPage.RemoveProduct();
+            var productPage = loginPage.LoginWithCreds("standard_user", "secret_sauce")
+                .AddProductToCard()
+                .RemoveProduct();
 
             productPage.ElementsAreAqualAssert(productPage.GetAddToCartButtonText(), "ADD TO CART");
         }
 
         [Test]
+        [Description("The cart page is opened after click on cart icon")]
         public void WhenAddItemAndClickOnCartIconShouldOpenCartPage()
         {
             var loginPage = new LoginPage(Driver);
-            var productPage = loginPage.LoginWithCreds("standard_user", "secret_sauce");
+            var productPage = loginPage.LoginWithCreds("standard_user", "secret_sauce")
+                .AddProductToCard();
 
-            productPage.AddProductToCard();
-            productPage.ClickOnCartIcon();
-            productPage.AppearRemoveButton();
+            var cartPage = productPage.ClickOnCartIcon()
+                .AssertAddedProductIsEqualToProductInTheCart(productPage._nameProduct.Text);
         }
     }
 }
+
 
