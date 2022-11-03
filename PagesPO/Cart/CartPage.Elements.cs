a9f0d9973b6a94d33a698df5fe03615f92ee27ef
Natalia Shubina <nshubinaqa@gmail.com>
2022-11-03 12:43:04 +0200

    Add new locators

--- PagesPO/Cart/CartPage.Elements.cs
+++ PagesPO/Cart/CartPage.Elements.cs
@@ -1,6 +1,15 @@
-﻿namespace SwagLabsProject.PagesPO.Cart
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SwagLabsProject.PagesPO.Cart
 {
     public partial class CartPage
     {
+        [FindsBy(How = How.ClassName, Using = "header_secondary_container")]
+        private IWebElement _headerTitle;
+
+        [FindsBy(How = How.Id, Using = "item_4_title_link")]
+        private IWebElement _addedNameProduct;
+
     }
 }
