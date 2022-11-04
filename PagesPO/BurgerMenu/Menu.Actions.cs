7e8045c46a880b51921a7050a81172aca26a89cb
Natalia Shubina <nshubinaqa@gmail.com>
2022-11-04 12:31:14 +0200

    Add menu folder to PagePO folder Add methods and elements to menu class

---
+++ PagesPO/BurgerMenu/Menu.Actions.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using SwagLabsProject.PagesPO.Login;
+
+namespace SwagLabsProject.PagesPO.BurgerMenu
+{
+    public partial class Menu
+    {
+        private IWebDriver _driver;
+        public Menu(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public LoginPage ClickLogout()
+        {
+            _btnLogout.Click();
+            return new LoginPage(_driver);
+        }
+    }
+}
---
+++ PagesPO/BurgerMenu/Menu.Elements.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SwagLabsProject.PagesPO.BurgerMenu
+{
+    public partial class Menu
+    {
+        [FindsBy(How = How.Id, Using = "logout_sidebar_link")]
+        private IWebElement _btnLogout;
+    }
+}
